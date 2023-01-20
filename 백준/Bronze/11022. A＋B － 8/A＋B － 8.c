80da7e124b59f8727babd49981345cf782be8542
JOON0512 <104539446+JOON0512@users.noreply.github.com>
2023-01-21 08:32:16 +0900

    [Bronze V] Title: A+B - 8, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int T, x, y;
+    scanf("%d", &T);
+    for(int i = 1; i <= T; i++){
+        scanf("%d %d", &x, &y);
+        printf("Case #%d: %d + %d = %d\n", i, x, y, x + y);
+    }
+
+    return 0;
+}
