82d8eb1dec0386abd2d11ca3bb0e471db7960999
JOON0512 <104539446+JOON0512@users.noreply.github.com>
2023-04-07 14:23:52 +0900

    [Silver IV] Title: 괄호, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.c
+++ 백준/Silver/9012. 괄호/괄호.c
@@ -1,105 +1,41 @@
-#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
-typedef struct node
-{
-	struct node* next;
-	int data;
-}Node;
+int is_valid(char * input){
+    int left = 0;
+    char* cursor = input;
 
-typedef struct stack
-{
-	Node* head;
-	int num;
-}Stack;
+    while(*cursor != '\0'){
+        if(*cursor == '('){
+            left++;
+        }
+        else{
+            left--;
+        }
+        if(left <0){
+            return 0;
+        }
+        cursor++;
+    }
 
-void Init(Stack* s);
-void PUSH(Stack* s, char data);
-int POP(Stack* s);
-int EMPTY(Stack* s);
-int TOP(Stack* s);
-
-int main()
-{
-	Stack s;
-	int t, n;
-	char str[52];
-	scanf("%d", &t);
-	getchar();
-
-	for (int i = 0; i < t; i++)
-	{
-		Init(&s);
-		fgets(str, sizeof(str), stdin);
-		str[strlen(str) - 1] = '\0';        // scanf("%s", str);
-
-		PUSH(&s, str[0]);
-
-		for (int j = 1; j < strlen(str); j++)
-		{
-			if (TOP(&s) == '(' && str[j] == ')')
-				POP(&s);
-			else
-				PUSH(&s, str[j]);
-		}
-
-		if (EMPTY(&s))
-			printf("YES\n");
-		else
-			printf("NO\n");
-	}
-
-
-	return 0;
+    if(left == 0){
+        return 1;
+    }
+    else{
+        return 0;
+    }
 }
 
-void Init(Stack* s)
-{
-	s->head = NULL;
-	s->num = 0;
-}
+int main(){
 
-void PUSH(Stack* s, char data)
-{
-	Node* newNode = (Node*)malloc(sizeof(Node));
-	newNode->data = data;
+    int iter;
+    char input[51];
+    
+    scanf("%d\n", &iter);
 
-	newNode->next = s->head;
-	s->head = newNode;
+    while(iter--){
+        scanf("%s", input);
+        is_valid(input) ? printf("YES\n") : printf("NO\n");
+    }
 
-	s->num++;
-}
-
-int POP(Stack* s)
-{
-	if (EMPTY(s))
-		return -1;
-
-	Node* rnode = s->head;
-	int Data = rnode->data;
-
-	s->head = s->head->next;
-
-	free(rnode);
-	s->num--;
-
-	return Data;
-}
-
-int EMPTY(Stack* s)
-{
-	if (s->head == NULL)
-		return 1;
-	else
-		return 0;
-}
-
-int TOP(Stack* s)
-{
-	if (EMPTY(s))
-		return -1;
-
-	return s->head->data;
+    return 0;
 }
