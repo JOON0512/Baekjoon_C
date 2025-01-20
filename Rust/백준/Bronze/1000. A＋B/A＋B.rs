1d8bcfe2dacecfe565bc516b9048b31cba8a37dd
nebelungen <104539446+JOON0512@users.noreply.github.com>
2025-01-20 23:28:15 +0900

    [Bronze V] Title: A+B, Time: 0 ms, Memory: 13204 KB -BaekjoonHub

--- Rust/백준/Bronze/1000. A＋B/A＋B.rs
+++ Rust/백준/Bronze/1000. A＋B/A＋B.rs
@@ -2,13 +2,13 @@ use std::io;
 
 fn main() {
     let mut input = String::new();
-    io::stdin().read_line(&mut input).unwrap(); // Read the entire line
+    io::stdin().read_line(&mut input).unwrap();
     let nums: Vec<i32> = input
-        .split_whitespace() // Split the line by any whitespace
-        .map(|num| num.parse().unwrap()) // Parse each part as an integer
+        .split_whitespace()
+        .map(|num| num.parse().unwrap())
         .collect();
 
     if nums.len() >= 2 {
-        println!("{}", nums[0] + nums[1]); // Output the sum of the first two numbers
+        println!("{}", nums[0] + nums[1]);
     }
 }
