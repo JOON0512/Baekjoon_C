5f3fedf92c13a96cc59d27e194cc99eda5c7091b
nebelungen <104539446+JOON0512@users.noreply.github.com>
2025-01-21 22:30:24 +0900

    [Bronze V] Title: ??!, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/10926. ？？！/？？！.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+int main() {
+    std::string input;
+    
+    while (std::getline(std::cin, input)){
+        std::cout << input << "??!" << std::endl;
+    }
+    
+    return 0;
+}
